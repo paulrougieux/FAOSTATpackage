8c164f42505804f2050c07cee52fcd6026bdda76
Sebastian Campbell <sebastian-c@users.noreply.github.com>
2023-02-18 14:54:35 +0100

    Modified getFAO to use new FAOSTAT API

--- FAOSTAT/R/getFAO.R
+++ FAOSTAT/R/getFAO.R
@@ -1,121 +1,96 @@
-##' Access to FAO FAOSTAT API.
-##'
-##' A function to access FAOSTAT data through the FAOSTAT API.
-##'
-##' Need to account for multiple itemCode, currently only support one
-##' single variable.
-##'
-##' @param name The name to be given to the variable.
-##' @param domainCode The domain of the data.
-##' @param elementCode The code of the element.
-##' @param itemCode The code of the specific item.
-##' @param yearRange A numeric vector containing the years to be downloaded.
-##' @param countrySet The FAOSTAT codes of those countries to be downloaded.
-##' @param query The object created if using the FAOsearch function.
-##' @param printURL Whether the url link for the data should be printed.
-##' @param useCHMT logical, whether the CHMT function should be
-##' applied to avoid double counting of China.
-##' @param outputFormat The format of the data, can be 'long' or 'wide'.
-##' @param returnNames Logical, should the area, the element and the
-##' item names be reported?.
-##' @param returnFlags, Logical, whether the flags should be
-##' returned. Only work with outputFormat long.
-##' @return Outputs a data frame containing the specified data.
-##' @export
-##' 
-##'
-##' @seealso \code{\link{getWDI}}, \code{\link{getWDItoSYB}},
-##' \code{\link{getFAOtoSYB}}, \code{\link{FAOsearch}}
-##'
+#' Access FAOSTAT API
+#'
+#' Uses the same functionality as the web interface to pull data from the
+#' FAOSTAT API. Contains most of its parameters. Currently only works for
+#' datasets that have area, item, element and year. Values for Chinese countries
+#' are not yet deduplicated.
+#'
+#' @param area_codes character. FAOSTAT area codes
+#' @param element_codes character. FAOSTAT element codes
+#' @param item_codes character. FAOSTAT item codes
+#' @param year character. Vector of desired years
+#' @param area_format character. Desired area code type in output (input still
+#'   needs to use FAOSTAT codes)
+#' @param item_format character. Item code
+#' @param dataset character. FAO dataset desired, e.g. RL, FBS
+#' @param metadata_cols character. Metadata columns to include in output
+#' @param include_na logical. Whether to include NAs for combinations of dimensions with no data
+#' @param language character. 2 letter language code for output labels
+#' @param base_url character. Base URL for API
+#' 
+#' @return data.frame in long format (wide not yet supported). Contains attributes for the URL and parameters used.
+#' @export
 
-getFAO = function(name = NULL, domainCode = "RL", elementCode = 5110,
-    itemCode = 6621, query, printURL = FALSE,
-    useCHMT = TRUE, outputFormat = "wide", returnNames = FALSE,
-    returnFlags = FALSE, yearRange = NULL, countrySet = NULL){
-    
-    ## Year range
-    if (!is.null(yearRange)) {
-      if (!is.numeric(yearRange)) {
-        stop("Please, provide a numeric vector for the year range.")
-      } else {
-        yearRange = paste(yearRange, collapse = ":")
-      }
+getFAO <- function(area_codes, element_codes, item_codes, year_codes, 
+                   area_format = c("M49", "FAO", "ISO2", "ISO3"),
+                   item_format = c("CPC", "FAO"),
+                   dataset = "RL", 
+                   metadata_cols = c("codes", "units", "flags", "notes"),
+                   include_na = FALSE,
+                   language = c("en", "fr", "es"),
+                   base_url = "https://fenixservices.fao.org/faostat/api/v1/"){
+  
+  coll <- function(string){
+    paste0(string, collapse = ",")
+  }
+  
+  language = match.arg(language, several.ok = FALSE)
+  area_format = match.arg(area_format, several.ok = FALSE)
+  
+  area_coll     = coll(area_codes)
+  item_coll     = coll(item_codes)
+  element_coll  = coll(element_codes)
+  year_coll     = coll(year_codes)
+  
+  params <- list(
+    area = area_coll, #These are FAO codes even if displayed in M49
+    area_cs = area_format,
+    element = element_coll,
+    item = item_coll, #I think these are FAO codes too, but they _happen_ to match for CPC
+    item_cs = item_format,
+    year = year_coll,
+    show_codes = "codes" %in% metadata_cols,
+    show_unit = "units" %in% metadata_cols,
+    show_flags = "flags" %in% metadata_cols,
+    show_notes = "notes" %in% metadata_cols,
+    null_values = include_na,
+    # pivot = FALSE,
+    # no_records = return_n_records_only,  # Used to only return the number of records
+    # page_number = 1,   # For pagination
+    # page_size = 100,   # Number of records for pagination
+    datasource = "DB4",
+    output_type = "objects" #csv is also a possibility
+    #latest_year = 1 # Will give the latest year available
+  )
+  
+  #Call to check for no records then get data
+  
+  final_url <- paste0(base_url, language, "/data/", dataset)
+  
+  resp <- GET(final_url, query = params)
+  
+  
+  if (status_code(resp) != 200) {
+    if (status_code(resp) == 404) {
+      stop("Error 404 - resource not found. Is something wrong with the URL?")
     }
-    ## Country set
-    if (!is.null(countrySet)) {
-      if (!is.numeric(countrySet)) {
-        stop("Please, provide a numeric vector for countrySet.")
-      } else {
-        countrySet = paste(countrySet, collapse = ":")
-      }
-    }
-    ## Query
-    if(!missing(query)){
-        if(NROW(query) > 1)
-            stop("Use 'getFAOtoSYB' for batch download")
-        domainCode = query$domainCode
-        itemCode = query$itemCode
-        elementCode = query$elementCode
-        if(is.null(query$name)){
-            name = with(query, paste(domainCode, itemCode, elementCode, sep = "_"))
-        } else {
-            name = query$name
-        }
-    }
-    ## Name
-    if(is.null(name))
-        name = paste(domainCode, itemCode, elementCode, sep = "_")
-
-    base = c("http://faostat3.fao.org/wds-r/api?")
-    database = "db=faostat4&"
-    selection = "select=A.AreaCode[FAOST_CODE],D.year[Year],D.value[Value]"
-    from = "&from=data[D],element[E],item[I],area[A]&"
-    condition = paste0("where=D.elementcode(", elementCode, "),D.itemcode(",
-        itemCode, "),D.domaincode('", domainCode, "')")
-    if (!is.null(yearRange)) {
-      condition = paste0(condition, ",D.year(", yearRange, ")")
-    }
-    if (!is.null(countrySet)) {
-      condition = paste0(condition, ",A.AreaCode(", countrySet, ")")
-    }
-    join = ",JOIN(D.elementcode:E.elementcode),JOIN(D.itemcode:I.itemcode),JOIN(D.areacode:A.areacode)&orderby=E.elementnamee,D.year"
-
-    ## Flags
-    if(returnFlags){
-        outputFormat = "long"
-        selection = paste0(selection, ",D.Flag[Flags]")
-    }
-    ## Names
-    if(returnNames)
-        selection = paste0(selection, "A.AreaNameE[AreaName],E.elementnamee[ElementName],I.itemnamee[ItemName]")
-    ## Call
-    out = "out=csv&"
-    url = paste0(base, out, database, selection, from, condition, join)
-    if(printURL)
-        print(url)
-
-    ## Allowing multiple server if any failed.
-    for(i in 1:length(url)){
-        faoData = suppressWarnings(try(read.csv(file = url[i],
-            stringsAsFactors = FALSE), silent = TRUE))
-        if(!inherits(faoData, "try-error"))
-            break
-    }
-    faoData$FAOST_CODE = as.integer(faoData$FAOST_CODE)
-    faoData$Year = as.integer(faoData$Year)
-    ## CHMT
-    if(useCHMT)
-        faoData = CHMT(var = "Value", data = faoData, year = "Year")
-    ## Output format
-    if(outputFormat == "long" & NROW(faoData) != 0){
-        faoData$domainCode = domainCode
-        faoData$itemCode = itemCode
-        faoData$elementCode = elementCode
-        faoData$name = name
-        faoData$Value <- as.numeric(gsub("n.a.", "", faoData$Value))
-    } else if(outputFormat == "wide"){
-        colnames(faoData)[colnames(faoData) == "Value"] = name
-        faoData[, name] <- as.numeric(gsub("n.a.", "", faoData[, name]))
-    }
-    faoData
+    stop("Request failed")
+  }
+  
+  resp_content <- content(resp)
+  
+  resp_list <- lapply(resp_content$data, as.data.frame)
+  
+  ret <- do.call(rbind, resp_list)
+  
+  #TODO Add China replacement function 
+  #TODO Replace this kludgy fix with something that provides an empty table
+  if(is.null(ret)){
+    stop("Data is empty")
+  }
+  
+  attr(ret, "url") <- final_url
+  attr(ret, "params") <- params
+  
+  return(ret)
 }
